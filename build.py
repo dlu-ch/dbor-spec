12af56f35b074a73227f86e7a1932900fd53de8f
Daniel Lutz <dlu-ch@users.noreply.github.com>
2020-06-25 17:27:59 +0200

    Setup dlb

---
+++ build.py
@@ -0,0 +1,43 @@
+# SPDX-License-Identifier: MIT
+# DBOR specification - Dense Binary Object Representation
+# Copyright (C) 2020 Daniel Lutz <dlu-ch@users.noreply.github.com>
+
+# Run this script by one of the following shell commands:
+#
+#    dlb build-all                  # from anywhere in the working tree (with directory of 'dlb' in $PATH)
+#    python3 -m build-all           # in the directory of this file
+#    python3 "$PWD"/build-all.py'   # in the directory of this file
+
+import sys
+
+import dlb.di
+import dlb.fs
+import dlb.ex
+import dlb_contrib.tex
+import dlb_contrib.iso6429
+
+
+if sys.stderr.isatty():
+    # assume terminal compliant with ISO/IEC 6429 ("VT-100 compatible")
+    dlb.di.set_output_file(dlb_contrib.iso6429.MessageColorator(sys.stderr))
+
+
+class PdfLatex(dlb_contrib.tex.Latex):
+    EXECUTABLE = 'pdflatex'
+    OUTPUT_EXTENSION = 'pdf'
+
+
+with dlb.ex.Context():
+    source_directory = dlb.fs.Path('doc/')
+    output_directory = dlb.fs.Path('build/out/')
+
+    # repeat redo until all state files exist and their content remains unchanged but at most 10 times
+    for i in range(10):
+        r = PdfLatex(toplevel_file=source_directory / 'dbor.tex',
+                     output_file=output_directory / 'dbor.pdf',
+                     input_search_directories=[source_directory / 'g/'],
+                     state_files=[output_directory / 'dbor.aux', output_directory / 'dbor.toc']).start()
+        if not r:
+            break
+
+dlb.di.inform('finished successfully')
